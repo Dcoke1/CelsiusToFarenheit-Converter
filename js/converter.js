f99b20d7931c8f8733535bd2b1ed20948e5bc2d9
Douglas Coke <CEO@Douglass-MacBook-Pro.local>
2019-03-22 21:56:23 -0400

    Final1.0

--- js/converter.js
+++ js/converter.js
@@ -1,9 +1,17 @@
-function converter(cel) {
+
+
+function input(event) {
+let value = event.target.value;
+let calc = ((value * (9/5)) + 32)
+document.getElementById("display").innerText = Math.ceil(calc);
+};
+
+/*function converter(cel) {
 let fahrenheit = (cel * (9/5)) + 32
 return fahrenheit;
-}
-converter();
+}; */
 
 
-console.log(converter(90));
+
+
 
