fd613a0ddf79ce6ad0895940ced452a1b51ed986
Douglas Coke <CEO@Douglass-MacBook-Pro.local>
2019-03-23 20:42:13 -0400

    Final1.1

--- js/converter.js
+++ js/converter.js
@@ -1,6 +1,6 @@
 
 /*----Takes users numeric input converts it to farenheit value then displays to display div in HTML ----*/
-function input(event) {
+let input = (event) => {
 let value = event.target.value;
 let calc = ((value * (9/5)) + 32)
 document.getElementById("display").innerText = Math.ceil(calc);
